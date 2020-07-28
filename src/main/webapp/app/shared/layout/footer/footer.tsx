73098dafe1a123508e6e2a6d0110c417ef2430a3
alex-bamovych <lexx_kataklizm_2010@mail.ru>
2020-07-28 11:12:49 +0300

    подготовка главной страницы

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -1,7 +1,6 @@
 import './footer.scss';
 
 import React from 'react';
-import { Translate } from 'react-jhipster';
 import { Col, Row } from 'reactstrap';
 
 const Footer = props => (
@@ -9,7 +8,6 @@ const Footer = props => (
     <Row>
       <Col md="12">
         <p>
-          <Translate contentKey="footer">Your footer</Translate>
         </p>
       </Col>
     </Row>
